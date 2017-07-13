c98eb7df355c867192c70e68d24dd9a90d39afee
Michal Janek <mikael.janek@gmail.com>
2017-07-13 16:11:42 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Lightbox code
+let $overlay = $("<div id='overlay'> </div>");
+let $img = $("<img>");
+let $caption = $("<p> </p>");
+$("body").append($overlay);
+$overlay.append($img);
+$overlay.append($caption);
+
+
+
+//Search aby cistil postupne obrazky
+//Add animations when filtering the gallery of photos
