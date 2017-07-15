baeb222d0534dd212fb27b04a30bdfb2aadb2e5d
Michal Janek <mikael.janek@gmail.com>
2017-07-15 05:11:47 +0200

    Update README.md,
    Finished sticky footer, filtering jQuery and README.md and prepared for
    submission.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Interactive gallery project
+Gallery features single-page project using [Lightbox2](http://lokeshdhakar.com/projects/lightbox2/) plugin and a custom jQuery filtering.<br>
+Pictures fade in and fade out according to user's input.
+## To do
+1. Setup a live demo:
+2. Fix HTML Markup to be more meaningful
+3. Use custom lightbox.
+4. Improve styling
+
+## Contact
+For any questions please feel free to contact me:<br />
+mikael.janek@gmail.com
--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-// Use manual filtering till you get it
+let $search = $('#search');
 
-// Profit.
+$(document).ready(function(){
+    $search.keyup(function(){
 
-// Fix markup issues it sux later on!
-//Lightbos firing plugin, changed maxWidth and resizing
+        // Retrieve the input field text
+        var filter = $(this).val();
+
+        // Loop through the comment list
+        $(".columns img").each(function(){
+
+            // If the list item does not contain the title attr, fade it out
+            if ($(this).attr("title").search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+
+                // Show the list item if the phrase matches
+            } else {
+                $(this).fadeIn();
+            }
+        });
+    });
+});
+
+//Lightbox firing plugin, changed maxWidth and resizing
 lightbox.option({
     'resizeDuration': 200,
     'maxWidth': 800,
